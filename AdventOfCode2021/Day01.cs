329a5d5f8ad1d73d8977138685a1bfd1fdd22296
Alexandra Almasan <alexandra_almasan27@yahoo.com>
2021-12-01 17:08:25 +0200

    Complete day 01

---
+++ AdventOfCode2021/Day01.cs
@@ -0,0 +1,11 @@
+﻿namespace AdventOfCode2021
+{
+    internal class Day01
+    {
+        public async Task<IList<int>> ReadData(string fileName) => (await File.ReadAllLinesAsync(fileName)).Select(int.Parse).ToList();
+
+        public long NumberOfDepthIncreases(IList<int> measurements) => measurements.Where((y, index) => index < measurements.Count - 1 && measurements[index + 1] > y).Count();   
+        
+        public long NumberOfSumDepthIncreases(IList<int> measurements) => NumberOfDepthIncreases(measurements.Select((x, index) => index < measurements.Count - 2 ? (x + measurements[index + 1] + measurements[index + 2]) : 0).ToList());    
+    }
+}
---
+++ AdventOfCode2021/Program.cs
@@ -0,0 +1,6 @@
+﻿using AdventOfCode2021;
+
+var day01 = new Day01();
+var measurements = await day01.ReadData("Day01.txt");
+Console.WriteLine(day01.NumberOfDepthIncreases(measurements));
+Console.WriteLine(day01.NumberOfSumDepthIncreases(measurements));
